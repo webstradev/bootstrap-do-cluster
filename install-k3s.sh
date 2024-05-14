af52ea9e261f3f7d0fa9109a85160d5bb5b00569
Erik Westra <e.s.westra.95@gmail.com>
2024-05-15 00:06:08 +0200

    adjust install script
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- install-k3s.sh
+++ install-k3s.sh
@@ -2,14 +2,10 @@
 
 if [ -z "$server_ip" ]; then
   # Install k3s server
-  curl -sfL https://get.k3s.io | K3S_TOKEN="${k3s_token}" sh -s - server
-  while ! test -d /etc/rancher/k3s/k3s.yaml; do
-    echo "Waiting for 'kubeconfig' file to be created at '/etc/rancher/k3s/k3s.yaml"
-    sleep 3
-  done
+  curl -sfL https://get.k3s.io | K3S_TOKEN="${k3s_token}" sh -
   # Adjust the kubeconfig permissions for easy retrieval
   chmod 644 /etc/rancher/k3s/k3s.yaml
 else
   # Install k3s agent
-  curl -sfL https://get.k3s.io | K3S_URL="https://${server_ip}:6443" K3S_TOKEN="${k3s_token}" sh -s -
+  curl -sfL https://get.k3s.io | K3S_URL="https://${server_ip}:6443" K3S_TOKEN="${k3s_token}" sh -
 fi
