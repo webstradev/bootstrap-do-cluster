080a5f7f56193fb95e9481c4c132a13f09747223
Erik Westra <e.s.westra.95@gmail.com>
2024-05-15 00:45:10 +0200

    small teardown fix
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- teardown-cluster.sh
+++ teardown-cluster.sh
@@ -1,4 +1,7 @@
-terraform apply -destroy -auto-approve
+terraform destroy -auto-approve
+# Need to do it again because the vpc might not have been removed if the 
+# previous command tried to destroy it before destroying all droplets.
+terraform destroy -auto-approve
 kubectl config delete-context k3s_dev_cluster
 kubectl config delete-cluster k3s_dev_cluster
 kubectl config delete-user k3s_dev_webstra 
