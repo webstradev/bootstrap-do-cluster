63dca57d06e61882228a67a9888a7e4b8e1f1392
Erik Westra <e.s.westra.95@gmail.com>
2024-05-15 18:56:35 +0200

    fix k3s token passing
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- install-k3s-server.sh
+++ install-k3s-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install k3s server
-curl -sfL https://get.k3s.io | K3S_TOKEN="${k3s_token}" sh - \
-  --tls-san ${k3s_lb_ip}
+curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} sh -s - \
+    --tls-san ${k3s_lb_ip}
 
 # Adjust the kubeconfig permissions for easy retrieval
 chmod 644 /etc/rancher/k3s/k3s.yaml
