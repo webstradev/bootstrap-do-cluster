c44de200a11832c3e0ad2583ab34557374eae7f0
Erik Westra <e.s.westra.95@gmail.com>
2024-05-15 09:27:30 +0200

    add sleep in teardown
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- teardown-cluster.sh
+++ teardown-cluster.sh
@@ -1,6 +1,7 @@
 terraform destroy -auto-approve
 # Need to do it again because the vpc might not have been removed if the 
 # previous command tried to destroy it before destroying all droplets.
+sleep 2;
 terraform destroy -auto-approve
 kubectl config delete-context k3s_dev_cluster
 kubectl config delete-cluster k3s_dev_cluster
