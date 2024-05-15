f1fc5f06a7bb36b2187c26f03e5e815b8ed2a57d
Erik Westra <e.s.westra.95@gmail.com>
2024-05-15 17:07:29 +0200

    add a loadbalancer for the k3s-api
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,45 +1,28 @@
-# resource "digitalocean_loadbalancer" "k3s_lb" {
-#   name   = "k3s-lb"
-#   region = var.region
+resource "digitalocean_loadbalancer" "k3s_lb" {
+  name     = "k3s-api-loadbalancer"
+  region   = var.region
+  vpc_uuid = digitalocean_vpc.k3s_vpc.id
 
-#   forwarding_rule {
-#     entry_port     = 80
-#     entry_protocol = "http"
-#     target_port    = 80
-#     target_protocol = "http"
-#   }
+  forwarding_rule {
+    tls_passthrough = true
+    entry_port      = 6443
+    entry_protocol  = "https"
 
-#   forwarding_rule {
-#     entry_port     = 443
-#     entry_protocol = "https"
-#     target_port    = 80
-#     target_protocol = "http"
-#     tls_passthrough = true
-#   }
+    target_port     = 6443
+    target_protocol = "https"
+  }
 
-#   healthcheck {
-#     port = 80
-#     protocol = "http"
-#     path = "/healthz"
-#   }
+  healthcheck {
+    port     = 6443
+    protocol = "tcp"
+  }
 
-#   droplet_ids = digitalocean_droplet.k3s_node.*.id
-# }
+  droplet_tag = digitalocean_tag.server.name
+}
 
-# resource "digitalocean_domain" "domain" {
-#   name = var.domain_name
-# }
-
-# resource "digitalocean_record" "a_record" {
-#   domain = digitalocean_domain.domain.name
-#   type   = "A"
-#   name   = "@"
-#   value  = digitalocean_loadbalancer.k3s_lb.ip
-# }
-
-# resource "digitalocean_record" "cname_record" {
-#   domain = digitalocean_domain.domain.name
-#   type   = "CNAME"
-#   name   = "www"
-#   value  = digitalocean_domain.domain.name
-# }
+resource "digitalocean_project_resources" "k3s_api_server_lb" {
+  project = digitalocean_project.k3s_cluster.id
+  resources = [
+    digitalocean_loadbalancer.k3s_lb.urn
+  ]
+}
