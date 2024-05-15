6439ddb20ff836156384e0f2ab940e169b9e5fc1
Erik Westra <e.s.westra.95@gmail.com>
2024-05-15 19:35:23 +0200

    remove k3s get node check
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- install-k3s-server.sh
+++ install-k3s-server.sh
@@ -4,8 +4,5 @@
 curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} sh -s - \
     --tls-san ${k3s_lb_ip}
 
-# Confirms the server is running
-k3s kubectl get node
-
 # Adjust the kubeconfig permissions for easy retrieval
 chmod 644 /etc/rancher/k3s/k3s.yaml
