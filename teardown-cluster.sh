e3d6dab4bf9023097f67a2c45fe4dffadd37d932
Erik Westra <e.s.westra.95@gmail.com>
2024-05-26 15:40:34 +0200

    dont rm on teardown (already do it on init)
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- teardown-cluster.sh
+++ teardown-cluster.sh
@@ -7,4 +7,3 @@ terraform destroy -auto-approve
 kubectl config delete-context k3s_dev_cluster
 kubectl config delete-cluster k3s_dev_cluster
 kubectl config delete-user k3s_dev_webstra 
-rm *.secret.yaml
