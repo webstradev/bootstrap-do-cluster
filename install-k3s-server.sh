6e7e14710273a7572b612b3dfbd142ace039dd07
Erik Westra <e.s.westra.95@gmail.com>
2024-06-12 21:42:06 +0200

    prepare for cilium cni
    Signed-off-by: Erik Westra <e.s.westra.95@gmail.com>

--- install-k3s-server.sh
+++ install-k3s-server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Install k3s server
-curl -sfL https://get.k3s.io | sh -s - --disable traefik
-
+curl -sfL https://get.k3s.io | sh -s - --disable traefik --flannel-backend=none --disable-network-policy
 # Adjust the kubeconfig permissions for easy retrieval
 chmod 644 /etc/rancher/k3s/k3s.yaml
